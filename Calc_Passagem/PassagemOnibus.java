8f58e9f2d058aa7eb1adfd7338063c24a93e06a5
adailtonsantosdeveloper <127420850+adailtonsantosdeveloper@users.noreply.github.com>
2023-04-26 08:43:08 -0300

    Não consegui o que queria
    Tentei criar o objeto Scanner para receber dados do usuário, mas não reconheceu

--- Calc_Passagem/PassagemOnibus.java
+++ Calc_Passagem/PassagemOnibus.java
@@ -1,13 +1,18 @@
-
+//import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class PassagemOnibus {
     public static void main(String[] args) {
-        System.out.println("Calcula passagem");
+       // Scanner entrada = new Scanner(System.in);
+        System.out.println("Esse App exibe o valor de 20 passagens");
+        System.out.println("Informe o valor unitário da passagem [use ponto ao invés de vírgula]");
+        String valPass = JOoptionPane.showInputDialog("Qual valor unitário da passagem? [use ponto no lugar da vírgula]");
+        double passagem = Double.parseDouble(valPass);
         double total = 0;
-        double passagem = 5.30;
         for (int cont = 0; cont < 20; cont++) {
             total = (cont+1) * passagem;
-            System.out.printf("%d passagem = [R$ %2.2f] %n",(cont+1),total);
+            System.out.printf("%d X %2.2f = [R$ %2.2f] %n", passagem,(cont+1),total);
         }
+       // entrada.close();
     }
     
 }
